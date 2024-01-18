4dc021ed2b8206a82c054d12c417fd4532cdc0b1
Otim Brian <otims.brian@gmail.com>
2024-01-18 04:41:13 +0300

    Created user and token extraction middleware

--- utils/middlewares.js
+++ utils/middlewares.js
@@ -1,4 +1,7 @@
-import logger from "./logger.js"
+import jwt from 'jsonwebtoken'
+import logger from "./logger.js";
+import { responseMessage } from './helper.js';
+import Employee from '../models/employee.js';
 
 // Error Handling Middeware.
 const errorHandler = (error, request, response, next) => {
@@ -31,8 +34,44 @@ const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
 
+
+// Middleware for Extrcting the user token.
+const tokenExtracter = (request, response, next) => {
+
+    // Get the uthorization header
+    const authorization  = request.get("authoriztion")
+
+    if(authorization && authorization.startsWith('Bearer ')){
+        // Get the token and assign it to the request.
+        request.token =  authorization.replace('Bearer ', '')
+    }else{
+        request.token = null
+    }
+    next()
+}
+
+// Middleware to extrct the user information from the token.
+const userExtractor = async (request, response, next) => {
+    
+    // Decode the token
+    const decodedToken = jwt.verify(request.token, process.env.SECRET_KEY)
+
+    // If there's no Id then the token is invalid.
+    if(!decodedToken.id){
+        const res = responseMessage(401, "missing or invalid token", null)
+        return response.status(401).send(res).end()
+    }
+
+    // Get the user and add it to the request.
+    request.user = await Employee.findById(decodedToken.id)
+
+    next()
+}
+
 export default {
     errorHandler,
     requestLogger,
-    unknownEndpoint
+    unknownEndpoint,
+    tokenExtracter,
+    userExtractor
 }
