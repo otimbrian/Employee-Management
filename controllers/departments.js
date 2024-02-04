439b320ce759ae1ecce2178136287fe229aa1182
Otim Brian <otims.brian@gmail.com>
2024-02-04 16:18:47 +0300

    Finished department backend

--- controllers/departments.js
+++ controllers/departments.js
@@ -7,53 +7,71 @@ import Employee from '../models/employee.js'
 const departmentRouter = express.Router()
 
 // Get all Departments.
-departmentRouter.get('/', middlewares.tokenExtracter, middlewares.checkAdmin, async (request, response) => {
-    const departments = await Department.find({}).populate('employees')
-    if (departments) {
-        response.json(departments)
+departmentRouter.get(
+    '/',
+    middlewares.tokenExtracter,
+    middlewares.checkAdmin,
+    async (request, response) => {
+        const departments = await Department.find({}).populate('employees')
+        if (departments) {
+            response.json(departments)
+        }
     }
-})
+)
 
 // Create a department.
-departmentRouter.post('/', middlewares.tokenExtracter, middlewares.checkAdmin, async (request, response) => {
-    const department = request.body
+departmentRouter.post(
+    '/',
+    middlewares.tokenExtracter,
+    middlewares.checkAdmin,
+    async (request, response) => {
+        const department = request.body
 
-    const newDepartment = new Department({
-        name: department.name
-    })
+        const newDepartment = new Department({
+            name: department.name
+        })
 
-    const savedDepartment = await newDepartment.save()
-    if (savedDepartment) {
-        response.status(201).json(savedDepartment)
-    } else {
-        const res = responseMessage(500, 'Error Saving Data to Datbase')
-        response.status(500).send(res)
+        const savedDepartment = await newDepartment.save()
+        if (savedDepartment) {
+            response.status(201).json(savedDepartment)
+        } else {
+            const res = responseMessage(500, 'Error Saving Data to Datbase')
+            response.status(500).send(res)
+        }
     }
-})
+)
 
 // Get a single deprtment using Id
-departmentRouter.get('/:id', middlewares.tokenExtracter, middlewares.checkAdmin, async (request, response) => {
-    // const departmentId = Number(request.params.id)
-    const department = await Department.findById(request.params.id)
+departmentRouter.get(
+    '/:id',
+    middlewares.tokenExtracter,
+    middlewares.checkAdmin,
+    async (request, response) => {
+        // const departmentId = Number(request.params.id)
+        const department = await Department.findById(request.params.id)
 
-    if (department) {
-        response.json(department)
-    } else {
-        const res = responseMessage(404, 'department not found in database')
-        response.status(404).send(res).end()
+        if (department) {
+            response.json(department)
+        } else {
+            const res = responseMessage(404, 'department not found in database')
+            response.status(404).send(res).end()
+        }
     }
-})
+)
 
 // Delete  department using Id.
-departmentRouter.delete('/:id', middlewares.tokenExtracter, middlewares.checkAdmin, async (request, response) => {
-    await Department.findByIdAndDelete(request.params.id)
-    response.status(204).end()
-})
+departmentRouter.delete(
+    '/:id',
+    middlewares.tokenExtracter,
+    middlewares.checkAdmin,
+    async (request, response) => {
+        await Department.findByIdAndDelete(request.params.id)
+        response.status(204).end()
+    }
+)
 
-//todo <-------- alot to be done here.
 // Update a department using id.
 departmentRouter.put('/:id', async (request, response) => {
-
     // Find the department by id
     const department = await Department.findById(request.params.id)
 
@@ -75,11 +93,8 @@ departmentRouter.put('/:id', async (request, response) => {
         })
     }
 
-
-     // Update change the department object to id to be updated to the database
-     request.body.employees = request.body.employees.map(
-        employee => employee.id
-    )
+    // Update change the department object to id to be updated to the database
+    request.body.employees = request.body.employees.map(employee => employee.id)
 
     const updatedDepartment = await Department.findByIdAndUpdate(
         request.params.id,
@@ -87,10 +102,9 @@ departmentRouter.put('/:id', async (request, response) => {
         { new: true }
     ).populate('employees')
 
-     // updating the employees to
+    // updating the employees to
     //  have the Ids of the updated department..
-     await employeesToBeUpdated.forEach(async employee => {
-        
+    await employeesToBeUpdated.forEach(async employee => {
         // Add the employee Id to the department as reference
         employee.department = employee.department.concat(department._id)
 
