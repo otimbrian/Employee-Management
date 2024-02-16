7173b37700bf45b863cfdfe6df1bb3677556d46b
Otim Brian <otims.brian@gmail.com>
2024-02-16 12:05:39 +0300

    Worked on user password change

--- controllers/employees.js
+++ controllers/employees.js
@@ -3,7 +3,7 @@ import bcrypt from 'bcrypt'
 import Employee from '../models/employee.js'
 import Department from '../models/department.js'
 import middleware from '../utils/middlewares.js'
-import { responseMessage } from '../utils/helper.js'
+import { hassPassword, responseMessage } from '../utils/helper.js'
 import logger from '../utils/logger.js'
 
 const employeeRouter = express.Router()
@@ -20,8 +20,7 @@ employeeRouter.post(
         const departmentsIds = []
         body.department.forEach(department => departmentsIds.push(department.id))
 
-        const saltRounds = 10
-        const passwordHass = await bcrypt.hash(body.password, saltRounds)
+        const passwordHass = await hassPassword(body.password)
 
         // Create a new Employee object
         const newEmployee = new Employee({
@@ -125,8 +124,8 @@ employeeRouter.delete(
 // Get one employee using id.
 employeeRouter.get(
     '/:id',
-    middleware.tokenExtracter,
-    middleware.checkAdmin,
+    // middleware.tokenExtracter,
+    // middleware.checkAdmin,
     async (request, response) => {
         const employee = await Employee.findById(request.params.id).populate(
             'department'
@@ -136,9 +135,39 @@ employeeRouter.get(
             response.status(200).json(employee)
         } else {
             const res = responseMessage(404, 'Employee not found in database', null)
-            response.status(404).send(res)
+            response.status(404).send(res).end()
         }
     }
 )
+
+// Change Employee Password.
+employeeRouter.put('/change-password', middleware.tokenExtracter, middleware.userExtractor, async (request, response) => {
+    const correctUserPassword = request.user ? false : bcrypt.compare(request.body.oldPassword, request.user.passwordHass)
+
+    if (!correctUserPassword){
+        const res = responseMessage(401, "Invalid Password", null)
+        response.status(401).send(res).end()
+    }
+
+    const newPasswordHass = hassPassword(request.body.newPassword)
+
+    const updatedUser = {
+        ...request.user,
+        passwordHass: newPasswordHass
+    }
+
+    const updatedEmployee = await Employee.findByIdAndUpdate(
+        request.user.id,
+        updatedUser,
+        { returnOriginal: false }
+    ).populate('department')
+
+    if (updatedEmployee) {
+        response.json(updatedEmployee.toJSON())
+    } else {
+        res = responseMessage(500, 'Failed to Update', null)
+        response.status(500).send(res)
+    }
+})
 
 export default employeeRouter
