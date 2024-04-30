c2911565508e7c0a3e43f3bf9df7fe77621ac623
otimbrian <otims.brian@gmail.com>
2024-04-30 06:57:00 +0300

    Made some changes

--- utils/middlewares.js
+++ utils/middlewares.js
@@ -1,98 +1,98 @@
-import jwt from 'jsonwebtoken'
-import logger from './logger.js'
-import { responseMessage } from './helper.js'
-import Employee from '../models/employee.js'
+import jwt from "jsonwebtoken";
+import logger from "./logger.js";
+import { responseMessage } from "./helper.js";
+import Employee from "../models/employee.js";
 
 // Error Handling Middeware.
 const errorHandler = (error, request, response, next) => {
-    console.error('Error message ===>', error.message)
+    console.error("Error message ===>", error.message);
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'incorrect/malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    } else if (error.name === 'TokenExpiredError') {
+    if (error.name === "CastError") {
+        return response.status(400).send({ error: "incorrect/malformatted id" });
+    } else if (error.name === "ValidationError") {
+        return response.status(400).json({ error: error.message });
+    } else if (error.name === "TokenExpiredError") {
         //If token is expired
         return response.status(401).json({
-            error: 'token expired'
-        })
-    } else if (error.name === 'JsonWebTokenError') {
+            error: "token expired",
+        });
+    } else if (error.name === "JsonWebTokenError") {
         //If token is missing or invalid.
-        return response.status(401).json({ error: error.message })
+        return response.status(401).json({ error: error.message });
     }
-    next(error)
-}
+    next(error);
+};
 
 // Middleware for
 // Logging the contents of a request.
 const requestLogger = (request, response, next) => {
-    logger.infor('Method:', request.method)
-    logger.infor('Path:  ', request.path)
-    logger.infor('Body:  ', request.body)
-    logger.infor('---')
-    next()
-}
+    logger.infor("Method:", request.method);
+    logger.infor("Path:  ", request.path);
+    logger.infor("Body:  ", request.body);
+    logger.infor("---");
+    next();
+};
 
 // Middleware for handling unknown or unhandled responses
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-}
+    response.status(404).send({ error: "unknown endpoint" });
+};
 
 // Middleware for Extrcting the user token.
 const tokenExtracter = (request, response, next) => {
-    logger.infor('token extraction middleware ....................')
+    logger.infor("token extraction middleware ....................");
     // logger.infor(request)
     // Get the authorization header
-    const authorization = request.get('Authorization')
+    const authorization = request.get("Authorization");
 
-    console.log('authorization ---->', authorization)
+    console.log("authorization ---->", authorization);
 
-    if (authorization && authorization.startsWith('Bearer ')) {
+    if (authorization && authorization.startsWith("Bearer ")) {
         // Get the token and assign it to the request.
-        const token = authorization.replace('Bearer ', '')
-        request.token = token
+        const token = authorization.replace("Bearer ", "");
+        request.token = token;
 
         // Decode the token.
-        const decodedToken = jwt.verify(token, process.env.SECRET_KEY)
+        const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
 
         // If there's no Id then the token is invalid.
         if (!decodedToken.id) {
-            const res = responseMessage(401, 'missing or invalid token', null)
-            return response.status(401).send(res).end()
+            const res = responseMessage(401, "missing or invalid token", null);
+            return response.status(401).send(res).end();
         }
 
         // Add the decoded token to the request.
-        request.decodedToken = decodedToken
+        request.decodedToken = decodedToken;
     } else {
-        request.token = null
+        request.token = null;
     }
-    next()
-}
+    next();
+};
 
 // Middleware to extrct the user information from the token.
 const userExtractor = async (request, response, next) => {
     // Get the user and add it to the request.
-    request.user = await Employee.findById(request.decodedToken.id)
+    request.user = await Employee.findById(request.decodedToken.id);
 
-    next()
-}
+    next();
+};
 
 // Admin Check middleware
 const checkAdmin = async (request, response, next) => {
-    console.log('Check Admin middleware............')
+    console.log("Check Admin middleware............");
     // console.log("Basic request ----->", request);
-    console.log('Decoded token ----->', request.decodedToken)
+    console.log("Decoded token ----->", request.decodedToken);
     if (!request.token) {
-        const res = responseMessage(401, 'missing token')
-        return response.status(401).send(res).end()
+        const res = responseMessage(401, "missing token");
+        return response.status(401).send(res).end();
     }
     if (!request.decodedToken.isAdmin) {
-        const res = responseMessage(401, 'action unauthorised', null)
-        return response.status(401).send(res).end()
+        const res = responseMessage(401, "action unauthorised", null);
+        return response.status(401).send(res).end();
     }
 
-    next()
-}
+    next();
+};
 
 export default {
     errorHandler,
@@ -100,5 +100,5 @@ export default {
     unknownEndpoint,
     tokenExtracter,
     userExtractor,
-    checkAdmin
-}
+    checkAdmin,
+};
